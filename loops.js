71f1eb97f381b8cf64afed28be6fb334697f2f80
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-02-09 22:06:50 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,4 +1,9 @@
 function forLoop(array);
-  for (var i =0; array.length; i++){
-    console.log(I am $([i] strange Loops);
+  for (let i =0; i <25, i++){
+  if (i == 1){
+    array.push('I am a 1 strange loop.')
+  } else {
+    array.push('I am ${i} strange loops')
+  }
 }
+return array;
