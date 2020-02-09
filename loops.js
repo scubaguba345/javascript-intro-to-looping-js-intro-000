cd19b6e4572f15723c6f8a8e9ea862ddadcea2aa
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-02-09 22:15:50 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -15,3 +15,13 @@ let countdown = n
   }
   return "done";
 }
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+
+  do {
+    array.pop();
+  } while (array.length > -1 && maybeTrue());
+    return array;
+}
